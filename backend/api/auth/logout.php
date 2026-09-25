a0662cf184ddfd95e4602cb13d560afc4809a744
SajithNuski <sajithnuski555@gmail.com>
2026-09-25 23:19:05 +0530

    Push 2: logout session cleanup

--- backend/api/auth/logout.php
+++ backend/api/auth/logout.php
@@ -13,13 +13,15 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 	respond_json(["success" => false, "error" => "Method not allowed"], 405);
 }
 
-$_SESSION = [];
-
-if (ini_get('session.use_cookies')) {
-	$params = session_get_cookie_params();
-	setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+if (empty($_SESSION['is_token_auth'])) {
+	$_SESSION = [];
+	if (ini_get('session.use_cookies')) {
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+	}
+	session_destroy();
+} else {
+	$_SESSION = [];
 }
-
-session_destroy();
 
 respond_json(["success" => true, "message" => "Logged out"]);
